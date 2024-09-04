37e17783c4862969b65fa064f64a6743fad443a5
Josue Arteaga <josuearteaga1515@gmail.com>
2024-09-04 13:50:32 -0700

    updated password

--- handlers/post.js
+++ handlers/post.js
@@ -4,7 +4,7 @@ const mysql = require("mysql");
 const con = mysql.createConnection({
     host: process.env.MYSQL_HOSTNAME,
     user: process.env.MYSQL_USERNAME,
-    password: "",
+    password: proccess.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE
 });
 
