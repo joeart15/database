d6224a95260a72577d5b0dcb22fd3d4b929ce2ff
Josue Arteaga <josuearteaga1515@gmail.com>
2024-09-04 14:03:02 -0700

    updated spelling

--- handlers/delete.js
+++ handlers/delete.js
@@ -4,7 +4,7 @@ const mysql = require("mysql");
 const con = mysql.createConnection({
     host: process.env.MYSQL_HOSTNAME,
     user: process.env.MYSQL_USERNAME,
-    password: proccess.env.MYSQL_PASSWORD,
+    password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE
 });
 
